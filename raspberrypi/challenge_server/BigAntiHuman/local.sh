92d596764e77577119a93a9175cc2d4888d0ce85
Dan Daggett <socialgeek@gmail.com>
2013-05-02 23:43:18 +0100

    Update BigAntiHuman/local.sh to reflect the change in port to 3001

--- raspberrypi/challenge_server/BigAntiHuman/local.sh
+++ raspberrypi/challenge_server/BigAntiHuman/local.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 tmp=$(mktemp -d)
-equ=$(curl -c "${tmp}/cookie.txt" "http://localhost:3000" 2>/dev/null | cut -d">" -f17 | cut -d"<" -f1)
+equ=$(curl -c "${tmp}/cookie.txt" "http://localhost:3001" 2>/dev/null | cut -d">" -f17 | cut -d"<" -f1)
 
 top=$[ ${equ:0:12} + ${equ:27:12} ]
 bot=$[ ${equ:12:12} + ${equ:39:12} ]
@@ -11,5 +11,5 @@ if [ ${#bot} -gt 12 ]; then
   top=$[$top + 1]
 fi
 
-curl --cookie "${tmp}/cookie.txt" --data-urlencode "val3=${top}${bot}" "http://localhost:3000" 2>/dev/null | cut -d":" -f2 | cut -d"<" -f1
+curl --cookie "${tmp}/cookie.txt" --data-urlencode "val3=${top}${bot}" "http://localhost:3001" 2>/dev/null | cut -d":" -f2 | cut -d"<" -f1
 
