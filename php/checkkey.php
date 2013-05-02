2d223863488ffc9a2993f33c496612f635456350
Dan Daggett <socialgeek@gmail.com>
2013-05-02 03:07:22 +0000

    Add final copy of PHP strcasecmp challenge.

--- php/checkkey.php
+++ php/checkkey.php
@@ -1,11 +1,14 @@
 <?php
 $key = "af4eed06aa4f49e8d3f2aec74ee8d95a";
 
+// Is truthiness even a word?
 if (0 == strcasecmp($key, $_GET['key'])) {
 	echo "The flag is : " . $key;
 }
 else {
-	echo "Try again":
+	echo "Try again";
 }
 ?>
+
+<p><a href="index.phps">View Source</a></p>
 
