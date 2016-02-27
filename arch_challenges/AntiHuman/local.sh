d41af0968c6f2f76ef332d2c8c9c93b771adc7d2
Ian Laird <ilaird@velma.com>
2016-02-26 20:21:33 -0700

    Variablized the 'target' in the test script

--- arch_challenges/AntiHuman/local.sh
+++ arch_challenges/AntiHuman/local.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-TMP=$(mktemp -d)
-val=$(curl -c "${TMP}/cookie.txt" "http://localhost:3000" 2>/dev/null | cut -d">" -f17 | cut -d"<" -f1 | awk '{print $1+$3}')
-curl --data-urlencode "val3=${val}" --cookie "${TMP}/cookie.txt" "http://localhost:3000" 2>/dev/null | cut -d":" -f2 | cut -d"<" -f1
-rm -r ${TMP}
-
+targ="http://localhost:3000"
+val=$[$(curl -c "cookie.txt" "${targ}" 2> /dev/null | cut -d'>' -f18 | cut -d'<' -f1)]
+curl --data-urlencode "val3=${val}" --cookie "cookie.txt" "${targ}" 2> /dev/null | cut -d":" -f2 | cut -d"<" -f1
